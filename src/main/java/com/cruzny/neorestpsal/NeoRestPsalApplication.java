29ee0a1901114046b44ec600e1bc174147bb490d
Alex Cruz <alexcruz182@gmail.com>
2021-10-06 19:38:05 -0400

    init commit

---
+++ src/main/java/com/cruzny/neorestpsal/NeoRestPsalApplication.java
@@ -0,0 +1,13 @@
+package com.cruzny.neorestpsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoRestPsalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoRestPsalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cruzny/neorestpsal/NeoRestPsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruzny.neorestpsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoRestPsalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
