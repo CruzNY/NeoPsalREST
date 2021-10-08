0d36cc3713b206c2245be89810a1fa91d880847e
Alex Cruz <alexcruz182@gmail.com>
2021-10-07 20:20:22 -0400

    creating school

---
+++ src/main/java/com/cruzny/neorestpsal/school/School.java
@@ -0,0 +1,33 @@
+package com.cruzny.neorestpsal.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class School {
+    @Id
+    @SequenceGenerator(
+            name = "school_sequence",
+            sequenceName = "school_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "school_sequence"
+    )
+    private Long id;
+    private String name;
+    private String streetAddress;
+    private String state;
+    private String city;
+
+    // Other school related information.
+}
